e0f15e702f7455942de19b70b8572f940a996116
Joseph Cutrono <joseph_cutrono@ultimatesoftware.com>
2016-12-08 02:44:33 -0500

    deploying just web

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	http.Handle("/api/", router)
 
-	http.Handle("/", UrlInspector(http.FileServer(http.Dir("./web/"))))
+	http.Handle("/", http.FileServer(http.Dir("./web/")))
 
 	var port string
 	if port = os.Getenv("PORT"); port == "" {
@@ -40,24 +40,4 @@ func initializeApi(router *mux.Router) {
 	apiRoutes.Headers("Content-Type", "application/json")
 
 	fmt.Println("API Router initialized")
-}
-
-func UrlInspector(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-
-		key := req.URL.Query().Get("key")
-
-		client := &http.Client{}
-		keyReq, _ := http.NewRequest("GET", "https://gwebproxy.ultilabs.xyz/api/key/"+key, nil)
-		keyReq.Header.Add("Referer", "https://gweb.ultilabs.xyz")
-		keyRes, _ := client.Do(keyReq)
-
-		if keyRes.StatusCode == http.StatusOK {
-			h.ServeHTTP(resp, req)
-		}
-
-		fmt.Printf("Resp: %+v", keyRes)
-
-		http.Error(resp, "invalid key", http.StatusUnauthorized)
-	})
 }
